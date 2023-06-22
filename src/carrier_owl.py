658f0393e9a3261477357989d9395a25e348282a
soonyan0718 <41360146+soonyan0718@users.noreply.github.com>
2023-06-22 18:47:51 +0900

    Update carrier_owl.py

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -191,7 +191,7 @@ def main():
                            max_results=1000,
                            sort_by='submittedDate',
                            iterative=False)
-    results = search_keyword(articles)
+    results = search_keyword(articles, keywords, score_threshold)
 
     slack_id = os.getenv("SLACK_ID") or args.slack_id
     line_token = os.getenv("LINE_TOKEN") or args.line_token
