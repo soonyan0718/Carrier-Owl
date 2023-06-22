a21518c2cb8ae1401dca1b03381b4051f3668f66
soonyan0718 <41360146+soonyan0718@users.noreply.github.com>
2023-06-22 19:20:10 +0900

    Update carrier_owl.py

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -56,16 +56,6 @@ def search_keyword(
         url = article['arxiv_url']
         title = article['title']
         abstract = article['summary']
-        score, hit_keywords = calc_score(abstract, keywords)
-        if (score != 0) and (score >= score_threshold):
-            title_trans = get_translated_text('ja', 'en', title, driver)
-            abstract = abstract.replace('\n', '')
-            abstract_trans = get_translated_text('ja', 'en', abstract, driver)
-            # abstract_trans = textwrap.wrap(abstract_trans, 40)  # 40行で改行
-            # abstract_trans = '\n'.join(abstract_trans)
-            result = Result(
-                    url=url, title=title_trans, abstract=abstract_trans,
-                    score=score, words=hit_keywords)
             results.append(result)
     
     # ブラウザ停止
